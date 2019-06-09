bb84edc19fa1a5463f2c8c1f6ae6db36e39fd9e3
Sharon Yao <sharon911225@gmail.com>
2019-06-08 20:58:33 -0400

    load & printed head of data

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello, Roary!')
+import pandas as pd
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+restaurant = pd.read_csv('data/Restaurants_in_Wake_County.csv')
+violation = pd.read_csv('data/Food_Inspection_Violations.csv')
+inspection = pd.read_csv('data/Food_Inspections.csv')
+
+
+print(restaurant.head())
+print(violation.head())
+print(inspection.head())
